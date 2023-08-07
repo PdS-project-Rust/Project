2e5581fc358cea66dc17f36ceac644fb7f7f07f4
Michele2301 <micheleferrero9@gmail.com>
2023-08-07 16:28:06 +0200

    fixed hotkey_module.rs

--- src/hotkey_module.rs
+++ src/hotkey_module.rs
@@ -20,68 +20,68 @@ pub mod hotkey_module{
     }
     pub struct HotkeyManager{
         manager:GlobalHotKeyManager,
-        quick_screenshot:Option<HotKey>,
-        new_screenshot:Option<HotKey>,
-        save:Option<HotKey>,
-        pen:Option<HotKey>,
-        rubber:Option<HotKey>,
+        quick_screenshot:(Option<HotKey>,bool),
+        new_screenshot:(Option<HotKey>,bool),
+        save:(Option<HotKey>,bool),
+        pen:(Option<HotKey>,bool),
+        rubber:(Option<HotKey>,bool),
     }
     impl HotkeyManager {
         pub fn new()->Result<Self,Box<dyn Error>>{
             Ok(HotkeyManager{
                 manager:GlobalHotKeyManager::new()?,
-                quick_screenshot:None,
-                new_screenshot:None,
-                save:None,
-                pen:None,
-                rubber:None,
+                quick_screenshot:(None,false),
+                new_screenshot:(None,false),
+                save:(None,false),
+                pen:(None,false),
+                rubber:(None,false),
             })
         }
         pub fn register_new_hotkey(&mut self, modifier:Option<Modifiers>,key:Code,key_type:KeyType)->Result<u32,Box<dyn Error>>{
             match key_type {
                 KeyType::Quick=>{
-                    if self.quick_screenshot.is_some() {
-                        self.manager.unregister(self.quick_screenshot.unwrap())?;
+                    if self.quick_screenshot.0.is_some() && self.quick_screenshot.1 == true{
+                        self.manager.unregister(self.quick_screenshot.0.unwrap())?;
                     }
                     let hk=HotKey::new(modifier,key);
                     self.manager.register(hk)?;
-                    self.quick_screenshot=Some(hk);
+                    self.quick_screenshot=(Some(hk),true);
                     Ok(hk.id())
                 },
                 KeyType::NewScreenshot=>{
-                    if self.new_screenshot.is_some() {
-                        self.manager.unregister(self.new_screenshot.unwrap())?;
+                    if self.new_screenshot.0.is_some() && self.new_screenshot.1 == true {
+                        self.manager.unregister(self.new_screenshot.0.unwrap())?;
                     }
                     let hk=HotKey::new(modifier,key);
                     self.manager.register(hk)?;
-                    self.new_screenshot=Some(hk);
+                    self.new_screenshot=(Some(hk),true);
                     Ok(hk.id())
                 },
                 KeyType::Save=>{
-                    if self.save.is_some() {
-                        self.manager.unregister(self.save.unwrap())?;
+                    if self.save.0.is_some() && self.save.1 == true {
+                        self.manager.unregister(self.save.0.unwrap())?;
                     }
                     let hk=HotKey::new(modifier,key);
                     self.manager.register(hk)?;
-                    self.save=Some(hk);
+                    self.save=(Some(hk),true);
                     Ok(hk.id())
                 },
                 KeyType::Pen=>{
-                    if self.pen.is_some() {
-                        self.manager.unregister(self.pen.unwrap())?;
+                    if self.pen.0.is_some() && self.pen.1 == true {
+                        self.manager.unregister(self.pen.0.unwrap())?;
                     }
                     let hk=HotKey::new(modifier,key);
                     self.manager.register(hk)?;
-                    self.pen=Some(hk);
+                    self.pen=(Some(hk),true);
                     Ok(hk.id())
                 },
                 KeyType::Rubber=>{
-                    if self.rubber.is_some() {
-                        self.manager.unregister(self.rubber.unwrap())?;
+                    if self.rubber.0.is_some() && self.rubber.1 == true {
+                        self.manager.unregister(self.rubber.0.unwrap())?;
                     }
                     let hk=HotKey::new(modifier,key);
                     self.manager.register(hk)?;
-                    self.rubber=Some(hk);
+                    self.rubber=(Some(hk),true);
                     Ok(hk.id())
                 },
             }
@@ -90,32 +90,37 @@ pub mod hotkey_module{
         pub fn disable_shortcut(&mut self, key_type:KeyType) -> Result<(),Box<dyn Error>> {
             return match key_type {
                 KeyType::Quick=>{
-                    if self.quick_screenshot.is_some(){
-                        self.manager.unregister(self.quick_screenshot.unwrap())?;
+                    if self.quick_screenshot.0.is_some() && self.quick_screenshot.1 == true{
+                        self.manager.unregister(self.quick_screenshot.0.unwrap())?;
+                        self.quick_screenshot.1==false;
                     }
                     Ok(())
                 },
                 KeyType::Pen=>{
-                    if self.pen.is_some(){
-                        self.manager.unregister(self.pen.unwrap())?;
+                    if self.pen.0.is_some() && self.pen.1 == true{
+                        self.manager.unregister(self.pen.0.unwrap())?;
+                        self.pen.1==false;
                     }
                     Ok(())
                 },
                 KeyType::NewScreenshot=>{
-                    if self.new_screenshot.is_some(){
-                        self.manager.unregister(self.new_screenshot.unwrap())?;
+                    if self.new_screenshot.0.is_some() && self.new_screenshot.1 == true{
+                        self.manager.unregister(self.new_screenshot.0.unwrap())?;
+                        self.new_screenshot.1==false;
                     }
                     Ok(())
                 },
                 KeyType::Rubber=>{
-                    if self.rubber.is_some(){
-                        self.manager.unregister(self.rubber.unwrap())?;
+                    if self.rubber.0.is_some() && self.rubber.1 == true{
+                        self.manager.unregister(self.rubber.0.unwrap())?;
+                        self.rubber.1==false;
                     }
                     Ok(())
                 },
                 KeyType::Save=>{
-                    if self.save.is_some(){
-                        self.manager.unregister(self.save.unwrap())?;
+                    if self.save.0.is_some() && self.save.1 == true{
+                        self.manager.unregister(self.save.0.unwrap())?;
+                        self.save.1==false;
                     }
                     Ok(())
                 }
@@ -124,32 +129,37 @@ pub mod hotkey_module{
         pub fn enable_shortcut(&mut self, key_type:KeyType) -> Result<(),Box<dyn Error>> {
             return match key_type {
                 KeyType::Quick=>{
-                    if self.quick_screenshot.is_some(){
-                        self.manager.register(self.quick_screenshot.unwrap())?;
+                    if self.quick_screenshot.0.is_some() && self.quick_screenshot.1 == false{
+                        self.manager.register(self.quick_screenshot.0.unwrap())?;
+                        self.quick_screenshot.1=true;
                     }
                     Ok(())
                 },
                 KeyType::Pen=>{
-                    if self.pen.is_some(){
-                        self.manager.register(self.pen.unwrap())?;
+                    if self.pen.0.is_some() && self.pen.1 == false{
+                        self.manager.register(self.pen.0.unwrap())?;
+                        self.pen.1=true;
                     }
                     Ok(())
                 },
                 KeyType::NewScreenshot=>{
-                    if self.new_screenshot.is_some(){
-                        self.manager.register(self.new_screenshot.unwrap())?;
+                    if self.new_screenshot.0.is_some() && self.new_screenshot.1 == false{
+                        self.manager.register(self.new_screenshot.0.unwrap())?;
+                        self.new_screenshot.1=true;
                     }
                     Ok(())
                 },
                 KeyType::Rubber=>{
-                    if self.rubber.is_some(){
-                        self.manager.register(self.rubber.unwrap())?;
+                    if self.rubber.0.is_some() && self.rubber.1 == false{
+                        self.manager.register(self.rubber.0.unwrap())?;
+                        self.rubber.1=true;
                     }
                     Ok(())
                 },
                 KeyType::Save=>{
-                    if self.save.is_some(){
-                        self.manager.register(self.save.unwrap())?;
+                    if self.save.0.is_some() && self.save.1 == false{
+                        self.manager.register(self.save.0.unwrap())?;
+                        self.save.1=true;
                     }
                     Ok(())
                 }
@@ -185,32 +195,32 @@ pub mod hotkey_module{
         pub fn get_key(&self,key_type:KeyType)->Option<u32>{
             return match key_type {
                 KeyType::Quick=>{
-                    if self.quick_screenshot.is_some(){
-                        return Some(self.quick_screenshot.unwrap().id());
+                    if self.quick_screenshot.0.is_some(){
+                        return Some(self.quick_screenshot.0.unwrap().id());
                     }
                     None
                 },
                 KeyType::Save=>{
-                    if self.save.is_some(){
-                        return Some(self.save.unwrap().id());
+                    if self.save.0.is_some(){
+                        return Some(self.save.0.unwrap().id());
                     }
                     None
                 },
                 KeyType::Rubber=>{
-                    if self.rubber.is_some(){
-                        return Some(self.rubber.unwrap().id());
+                    if self.rubber.0.is_some(){
+                        return Some(self.rubber.0.unwrap().id());
                     }
                     None
                 },
                 KeyType::Pen=>{
-                    if self.pen.is_some(){
-                        return Some(self.pen.unwrap().id());
+                    if self.pen.0.is_some(){
+                        return Some(self.pen.0.unwrap().id());
                     }
                     None
                 },
                 KeyType::NewScreenshot=>{
-                    if self.new_screenshot.is_some(){
-                        return Some(self.new_screenshot.unwrap().id());
+                    if self.new_screenshot.0.is_some(){
+                        return Some(self.new_screenshot.0.unwrap().id());
                     }
                     None
                 }
